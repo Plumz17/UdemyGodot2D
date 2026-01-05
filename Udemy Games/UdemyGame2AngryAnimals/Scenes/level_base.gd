6955456a68f07b22a4656a45f311011498ac7964
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2026-01-05 22:46:02 +0700

    Added Some Stuff

--- Udemy Games/UdemyGame2AngryAnimals/Scenes/level_base.gd
+++ Udemy Games/UdemyGame2AngryAnimals/Scenes/level_base.gd
@@ -3,8 +3,11 @@ extends Node2D
 const ANIMAL = preload("uid://cg0qs4dl53cop")
 
 func _ready() -> void:
+	SignalHub.on_animal_died.connect(spawn_animal)
 	spawn_animal()
-	SignalHub.on_animal_died.connect(spawn_animal) # Might bug and have to be added into a on enter tree
+	
+#func _enter_tree() -> void:
+	#SignalHub.on_animal_died.connect(spawn_animal) # Might bug and have to be added into a on enter tree
 	
 	
 func spawn_animal() -> void:
