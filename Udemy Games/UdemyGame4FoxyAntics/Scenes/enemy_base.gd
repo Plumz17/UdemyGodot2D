06655382cce3b917caf9f8f92098f0b3eac2481f
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2026-01-08 23:40:56 +0700

    Continued Third Game

---
+++ Udemy Games/UdemyGame4FoxyAntics/Scenes/enemy_base.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+class_name EnemyBase
+const FALL_OFF_Y: float = 200.0
+
+@export var points: int = 1
+@export var speed: float = 30
+var _gravity: float = 800
+var _player_ref: Player
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_player_ref = get_tree().get_first_node_in_group(Constants.PLAYER_GROUP)
+	if _player_ref == null:
+		queue_free()
+
+func die() -> void:
+	set_physics_process(false)
+	queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if global_position.y > FALL_OFF_Y:
+		queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	pass # Replace with function body.
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	die() 
--- Udemy Games/UdemyGame4FoxyAntics/Scenes/player.gd
+++ Udemy Games/UdemyGame4FoxyAntics/Scenes/player.gd
@@ -3,14 +3,19 @@ class_name Player
 
 @export var gravity: float = 690
 @export var jump_force: float = -270
-@export var run_speed: float = 120
+@export var run_speed: float = 150 
+@export var terminal_velocity: float = 350
+@export var fallen_off_y: float = 800
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var label: Label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _enter_tree() -> void:
+	add_to_group(Constants.PLAYER_GROUP)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -20,4 +25,18 @@ func _physics_process(delta: float) -> void:
 		velocity.y = jump_force
 	if not is_equal_approx(velocity.x, 0.0):
 		sprite_2d.flip_h = velocity.x < 0
+	velocity.y = clamp(velocity.y, jump_force, terminal_velocity)
 	move_and_slide()
+	update_debug_label()
+	fallen_off()
+
+func update_debug_label() -> void:
+	var ds: String = ""
+	ds += "Floor:%s\n" % is_on_floor()
+	ds += "V:%.1f,%.1f\n" % [velocity.x, velocity.y]
+	ds += "P:%.1f,%.1f" % [global_position.x, global_position.y]
+	label.text = ds
+
+func fallen_off() -> void:
+	if global_position.y > fallen_off_y:
+		queue_free()
